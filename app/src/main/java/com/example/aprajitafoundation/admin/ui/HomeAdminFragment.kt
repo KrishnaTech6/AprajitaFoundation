d44d1c60fec8a498950bada70e3cb87d16fb3c36
krishnaTech6 <krishna060702@gmail.com>
2024-08-29 10:50:48 +0530

    added ui to admin home , added del edit button , ui changes

--- app/src/main/java/com/example/aprajitafoundation/admin/ui/HomeAdminFragment.kt
+++ app/src/main/java/com/example/aprajitafoundation/admin/ui/HomeAdminFragment.kt
@@ -1,11 +1,18 @@
 package com.example.aprajitafoundation.admin.ui
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.aprajitafoundation.api.User
 import com.example.aprajitafoundation.databinding.FragmentHome2Binding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class HomeAdminFragment : Fragment() {
 
@@ -21,9 +28,21 @@ class HomeAdminFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHome2Binding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        val sharedPreferences = requireContext().getSharedPreferences("AppPreferences",MODE_PRIVATE )
+
+        val savedUserJson = sharedPreferences.getString("user", null)
+        val type = object : TypeToken<User>() {}.type
+        val savedUser = Gson().fromJson<User>(savedUserJson, type)
+
+
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault()) //Monday , 29 August 2024
+        val date = dateFormat.format(System.currentTimeMillis())
+
+        binding.welcomeText.text = "Welcome, ${savedUser.name}!"
+        binding.dateTimeText.text = "Today is $date"
+
+        return binding.root
     }
 
     override fun onDestroyView() {
